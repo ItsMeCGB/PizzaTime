772fa0538b93e622d30909190236e91b03393508
F0rtis5 <117367589+F0rtis5@users.noreply.github.com>
2023-11-30 11:54:07 -0600

    test message

---
+++ BlazorApp/Pizza.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+/*
+namespace BlazorApp
+{
+	public class Pizza
+	{
+		public Topping PizzaTopping { get; set; }
+
+		public Pizza()
+		{
+			 PizzaTopping = new Topping();
+		}
+
+		public decimal GetFinalPrice()
+		{
+			return PizzaTopping.ToppingPrice();
+		}
+	}
+
+	public class Topping {
+		public bool Bacon { get; set; }
+		public bool MoreBacon { get; set; }
+		public bool ExtraExtraBacon { get; set; }
+
+		public decimal ToppingPrice()
+		{
+			decimal runningTotal = 0;
+
+			if (Bacon)
+			{
+				runningTotal += 30;
+			}
+			if (MoreBacon)
+			{
+				runningTotal += 50;
+			}
+			if (ExtraExtraBacon)
+			{
+				runningTotal += 60;
+			}
+
+		}
+	}
+*/
